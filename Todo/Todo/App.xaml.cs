2339803959de85ad9f2176f9a8d2737dd8bb5878
Ashvin Bhuva <bhuva.ashvin01@gmail.com>
2022-04-23 19:01:17 +0530

    Add project files.

---
+++ Todo/Todo/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Todo.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Todo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new TodoListPage())
+            {
+                BarTextColor = Color.White,
+                BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"]
+            };
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
